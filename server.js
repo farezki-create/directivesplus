b4b055ca4d934a1475bc61f89123f14646180527
farezki-create <arezki_farid@hotmail.com>
2025-04-19 23:22:34 +0200

    add server configuration

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-app.use(express.static('dist'));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(__dirname + '/dist/index.html');
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
 });
